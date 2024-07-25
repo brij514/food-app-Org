a6325b7970be82b10a61a6306a5f08bc04eb927b
brij514 <brijmohan514600@gmail.com>
2024-07-26 01:06:56 +0530

    first-commit

---
+++ frontend/src/components/Header/Header.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import "./Header.css";
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="header-contents">
+        <h2>Order your favourite food here</h2>
+        <p>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Inventore
+          laboriosam a, expedita reprehenderit doloremque dicta magnam illum,
+          sed iste dolorem velit unde ipsa. Laudantium ducimus, recusandae ipsam
+          rem provident eaque possimus, praesentium suscipit similique nostrum
+          at officia maiores. Aut nulla id quas minima?
+        </p>
+        <button>View Menu</button>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
---
+++ frontend/src/pages/Verify/Verify.jsx
@@ -0,0 +1,38 @@
+import React, { useContext, useEffect } from "react";
+import "./Verify.css";
+import { useNavigate, useSearchParams } from "react-router-dom";
+import { StoreContext } from "../../context/StoreContext";
+import axios from "axios";
+
+const Verify = () => {
+  const [searchParams, setSearchParams] = useSearchParams();
+  const success = searchParams.get("success");
+  const orderId = searchParams.get("orderId");
+  const { url } = useContext(StoreContext);
+  const navigate = useNavigate();
+
+  const verifyPayment = async () => {
+    const response = await axios.post(url + "/api/order/verify", {
+      success,
+      orderId,
+    });
+    if (response.data.success) {
+      //If payment is done then redirect to myorders pag
+      navigate("/myorders");
+    } else {
+      //If payment failed then redirect to Home page
+      navigate("/");
+    }
+  };
+  useEffect(() => {
+    verifyPayment();
+  }, []);
+
+  return (
+    <div className="verify">
+      <div className="spinner"></div>
+    </div>
+  );
+};
+
+export default Verify;
