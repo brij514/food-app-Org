ddee0fe08f758aff3f7e5d20256ba56f68d1a1dd
Brijmohan <116019774+brij514@users.noreply.github.com>
2024-08-17 14:45:30 +0530

    Update Footer.jsx

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -31,7 +31,7 @@ const Footer = () => {
         </div>
       </div>
       <hr />
-      <p className="footer-copyright">Copyright 2024 © Tomato.com -All Right Reserved</p>
+      <p className="footer-copyright">Design & Developed by Brijmohan</p>
     </div>
   );
 };
